c9b302195f9f43a2c89c5c4c90ff1acf4115d15f
Joe Nyugoh <nyugoh@gmail.com>
2019-02-15 17:18:01 +0300

    Inital code for PWA

---
+++ assets/js/main.js
@@ -0,0 +1,35 @@
+/**
+ * Main.js file
+ * Used to bootstrap the app
+ * Has a one function, app that contains all the other functions
+ * @param { * }
+ * @returns {}
+ * */ 
+
+ (function(){
+    var app = {
+
+    }
+
+    /**
+     * Load data incase there's nothing on cache
+     *  */
+    app.loadInitialData = function() {
+        console.log("Initial data is empty");
+    };
+
+    app.showAddTodoModal = function() {
+        $('#myModal').on('shown.bs.modal', function () {
+            $('#myInput').trigger('focus')
+        });
+    };
+
+    app.deleteTask = function(taskId) {
+        console.log('Deleting task :', taskId);
+    };
+
+    app.markTaskAsComplete = function(taskId) {
+        console.log('Task :'+ taskId + ' is complete.');
+    };
+})()
+ 
